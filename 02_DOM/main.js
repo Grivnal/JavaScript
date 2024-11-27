87a0da172456342f33b3d2027169bf2402fd70b1
Grivnal <153536388+Grivnal@users.noreply.github.com>
2024-11-27 09:06:18 +0000

    changes

--- 02_DOM/main.js
+++ 02_DOM/main.js
@@ -1,3 +1,14 @@
-document.write("<p>Write content</p>")
+const tableBody = document.getElementById('table-body');
+const createBtn = document.getElementById('create-btn');
+const deleteBtn = document.getElementById('delete-btn');
 
-document.get
+tableBody.innerHTML = "";
+let products = [];
+
+function addProductToTable(product)
+{
+    tableBody.innerHTML += `<tr class="table-secondary">
+                            <th scope="row">${product.Fish}</th>
+                            <td>${product.Apple}</td>
+                            <td>${product.Egg}</td></tr>`; 
+}
